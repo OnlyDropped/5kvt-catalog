2ecbc940678422edd491587110fc16535f53fb6a
OnlyDropped <asheburenkov@yandex.ru>
2022-12-15 00:51:52 +0300

    add new sections

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@ $('.banner-slider').slick({
   nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.svg" alt=""></button>',
 
 })
+
+$('.products-content').slick({
+  arrows: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  // cssEase: 'ease-in-out',
+  // prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-prev.svg" alt=""></button>',
+  // nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.svg" alt=""></button>',  
+})
