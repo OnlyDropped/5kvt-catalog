885ed11b6ca9ac591170eb7b0804cacd07ef992f
OnlyDropped <asheburenkov@yandex.ru>
2022-12-26 00:43:44 +0300

    create contacts.html and add change in card.html

--- js/app.js
+++ js/app.js
@@ -19,3 +19,16 @@ $('.products-content').slick({
   nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.svg" alt=""></button>',  
 })
 
+
+      $('.card__number').styler();
+      $('.card-info__view').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        cssEase: 'ease-in-out',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.svg" alt=""></button>',
+      })
+
+
+
